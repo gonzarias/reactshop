4bf701e3a549ce1981979afdae880007f006a19a
gonzarias <gonzarias@gmail.com>
2022-02-28 19:37:43 -0300

    chore: add more limit to api

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     domains: ['placeimg.com']
   },
-}
+};
 
 
 module.exports = nextConfig
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -3,7 +3,7 @@ import ProductItem from '@components/ProductItem';
 import useGetProducts from '@hooks/useGetProducts';
 import styles from '@styles/ProductList.module.scss';
 
-const API = 'https://api.escuelajs.co/api/v1/products?limit=25&offset=1';
+const API = 'https://api.escuelajs.co/api/v1/products?limit=30&offset=1';
 
 const ProductList = () => {
 
